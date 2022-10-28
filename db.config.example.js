e35052bd054238eee6b1cea80124a07a6b52871c
anan <andyccoge@gmail.com>
2022-10-29 05:58:41 +0800

    多資料庫版、.htaccess

--- db.config.example.js
+++ db.config.example.js
@@ -1,9 +1,37 @@
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+const location_href = location.href;
+let db_connect = location_href.split('//')[1];
+db_connect = db_connect.split('.')[0];
+// alert(db_connect);
+
+export let firebaseConfig = {};
+if(db_connect=='127' || db_connect=='badminton-courts'){
+  firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+}
+else if(db_connect=='badminton-courts2-tutcn'){
+  firebaseConfig = {
+  };
+}
+else if(db_connect=='badminton-courts3-qizrtt'){
+  firebaseConfig = {
+  };
+}
+else if(db_connect=='badminton-courts4-ul4as'){
+  firebaseConfig = {
+  };
+}
+else if(db_connect=='badminton-courts5-olnbp'){
+  firebaseConfig = {
+  };  
+}
+else if(db_connect=='badminton-courts6-qttybe'){
+  firebaseConfig = {
+  };
+}
