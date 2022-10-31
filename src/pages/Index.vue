f498d4a53830366451098ed316e101f1793570f3
anan <andyccoge@gmail.com>
2022-10-31 22:56:18 +0800

    首頁添加說明

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,21 @@
 <script setup>
+  import { ref } from 'vue';
   import ModalAddhome from '../components/ModalAddhome.vue';
+  import * as Icon from '@heroicons/vue/24/outline';
+  import Modal from '../components/Modal.vue';
+
+  const modal_open_question = ref(false); 
 </script>
 
 <template>
   <ModalAddhome></ModalAddhome>
   <div class="h-screen flex flex-col items-center justify-center">
-    <h1 class="font-bold mb-5 text-3xl sm:text-5xl text-center">羽球排場系統</h1>
+    <h1 class="font-bold mb-5 text-3xl sm:text-5xl text-center">
+      羽球排場系統
+      <button class="rounded-2xl p-0.5 aspect-square border-2 mr-2 bg-yellow-300 border-yellow-600" @click="modal_open_question=true">
+        <Icon.QuestionMarkCircleIcon class="w-5 h-5"></Icon.QuestionMarkCircleIcon>
+      </button>
+    </h1>
     <div class="w-full grid sm:grid-cols-2 grid-cols-1 mb-20 sm:px-4 px-0">
       <div class="m-4 flex justify-center">
         <a class="group" href="/pages/court_manage.html">
@@ -35,6 +45,49 @@
       </div>
     </div>
   </div>
+
+  <!-- 操作說明 -->
+  <modal :show="modal_open_question" :click_bg_close="true" @close="modal_open_question=false">
+    <template #header>
+      <h3 class="font-bold text-xl flex items-center">
+        操作說明
+      </h3>
+    </template>
+    <template #body>
+      <div class="mb-2">
+        先簡單說明一下基本流程：
+        <ol>
+          <li>後台管理>打球日管理 新增打球日。</li>
+          <li>打球日新增完成後點ID的超連結或紫色小齒輪進入設定詳細內容。</li>
+          <li>設定場地，可新增多個比賽場及預備場，比賽場可輸入名稱，例如：場地A。</li>
+          <li>
+            設定人員，有三種設法：
+            <ul>
+              <li>單一設定：選擇已往建立過的人員。</li>
+              <li>單一新增：一個一個新增，可輸入性別、等級等資料。</li>
+              <li>批次設定：以英文逗號(,)分隔姓名，系統會去比對過去名單，存在則套用，不存在則新增。</li>
+            </ul>
+          </li>
+        </ol>
+      </div>
+      <div class="mb-2">
+        以上都完成後就可點時間/地點 區塊，操作的第二顆按鈕(四個小正方形)，或回首頁點安排比賽(抓最新的打球日)開始進行場地的人員安排~
+      </div>
+    </template>
+    <template #footer>
+      <div></div>
+    </template>
+  </modal>
 </template>
 
-<style scoped></style>
+<style scoped>
+  ol{
+    list-style: auto;
+  }
+  ul{
+    list-style: disc;
+  }
+  ol,ul{
+    padding: revert;
+  }
+</style>
